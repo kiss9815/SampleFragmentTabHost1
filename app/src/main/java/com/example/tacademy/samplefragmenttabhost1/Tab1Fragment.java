49eab29b7bd7434864cca97a244d7b6c9bdc2a5b
kiss9815 <kiss9815@gmail.com>
2016-01-26 14:06:48 +0900

    tabHost에서 프래그먼트에 프래그먼트를 추가해서 tabHost 사용

--- app/src/main/java/com/example/tacademy/samplefragmenttabhost1/Tab1Fragment.java
+++ app/src/main/java/com/example/tacademy/samplefragmenttabhost1/Tab1Fragment.java
@@ -3,6 +3,7 @@ package com.example.tacademy.samplefragmenttabhost1;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,12 +19,19 @@ public class Tab1Fragment extends Fragment {
         // Required empty public constructor
     }
 
+    FragmentTabHost tabHost;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab1, container, false);
+        tabHost = (FragmentTabHost)view.findViewById(R.id.tabHost);
+        tabHost.setup(getContext(), getChildFragmentManager(), android.R.id.tabcontent);
+
+        tabHost.addTab(tabHost.newTabSpec("child1").setIndicator("CHILD1"), Child1Tab1Fragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("child2").setIndicator("CHILD2"), Child2Tab1Fragment.class, null);
+        return view;
     }
 
 }
